e9757102c89418c8f89b60704e3d2121e002508c
Chung Min Kim <chungmin99@gmail.com>
2024-12-19 02:32:02 -0800

    init

---
+++ src/splax/_camera.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+from typing import Optional
+
+import jax
+import jax_dataclasses as jdc
+import jaxlie
+import jax.numpy as jnp
+
+from ._gaussian_splat import Gaussians
+
+
+@jdc.pytree_dataclass
+class Camera:
+    fx: jnp.ndarray
+    fy: jnp.ndarray
+    cx: jnp.ndarray
+    cy: jnp.ndarray
+    near: jnp.ndarray
+    far: jnp.ndarray
+    width: jdc.Static[int]
+    height: jdc.Static[int]
+    pose: jaxlie.SE3
+
+    @staticmethod
+    def from_intrinsics(
+        fx: jnp.ndarray,
+        fy: jnp.ndarray,
+        cx: jnp.ndarray,
+        cy: jnp.ndarray,
+        width: int,
+        height: int,
+        near: Optional[jnp.ndarray] = None,
+        far: Optional[jnp.ndarray] = None,
+        pose: Optional[jaxlie.SE3] = None,
+    ) -> Camera:
+        batch_axes = fx.shape
+        assert fx.shape == fy.shape == cx.shape == cy.shape == batch_axes
+        if near is None:
+            near = jnp.full(batch_axes, 0.1)
+        if far is None:
+            far = jnp.full(batch_axes, 1000.0)
+        if pose is None:
+            pose = jaxlie.SE3.identity(batch_axes=batch_axes)
+        return Camera(fx, fy, cx, cy, near, far, width, height, pose)
+
+    def projection_mat(self) -> jnp.ndarray:
+        batch_axes = self.fx.shape
+        P = jnp.zeros((*batch_axes, 4, 4))
+        P = P.at[..., 0, 0].set(2 * self.fx / self.width)
+        P = P.at[..., 1, 1].set(2 * self.fy / self.height)
+        P = P.at[..., 2, 2].set((self.far + self.near) / (self.far - self.near))
+        P = P.at[..., 2, 3].set(-2 * self.far * self.near / (self.far - self.near))
+        P = P.at[..., 3, 2].set(1)
+        return P
+
+    def affine_transform_jacobian(self, t: jnp.ndarray) -> jnp.ndarray:
+        batch_axes = t.shape[:-1]
+        J = jnp.zeros((*batch_axes, 2, 3))
+        J = J.at[..., 0, 0].set(self.fx / t[..., 2])
+        J = J.at[..., 1, 1].set(self.fy / t[..., 2])
+        J = J.at[..., 0, 2].set(-self.fx * t[..., 0] / t[..., 2] ** 2)
+        J = J.at[..., 1, 2].set(-self.fy * t[..., 1] / t[..., 2] ** 2)
+        return J
+
+    @jdc.jit
+    def project(self, gaussians: Gaussians) -> tuple[Gaussians, jnp.ndarray]:
+        """
+        Project 3D Gaussians to 2D, and also return depth from camera.
+        """
+        assert gaussians.get_and_check_shape() == 3
+        assert isinstance(gaussians.quat, jaxlie.SO3)
+
+        t = self.pose.inverse() @ gaussians.means
+
+        # If depth ~ 0, then projection to `t_d` becomes numerically unstable.
+        # Setting to arbitrary position is OK since we won't render if depth < 0.
+        t = jnp.where(t[..., 2:] < 1e-6, jnp.ones_like(t) * -1e-6, t)
+
+        t_d = jnp.einsum(
+            "...ij,...j->...i",
+            self.projection_mat(),
+            jnp.concatenate([t, jnp.ones((*t.shape[:-1], 1))], axis=-1),
+        )
+
+        mean_d = jnp.array(
+            [
+                (self.width * t_d[..., 0] / (t_d[..., -1]) + 1) / 2 + self.cx,
+                (self.height * t_d[..., 1] / (t_d[..., -1]) + 1) / 2 + self.cy,
+            ]
+        ).T
+
+        # Project 3D covariance to 2D.
+        J = self.affine_transform_jacobian(t)
+        R = gaussians.quat.as_matrix()
+        cov = jnp.einsum(
+            "...ij,...j,...jm->...im",
+            R,
+            jnp.diag(gaussians.scale**2),
+            R.swapaxes(-1, -2),
+        )
+        R_cw = self.pose.inverse().rotation().as_matrix()
+        cov_d = jnp.einsum(
+            "...ij,...jk,...kl,...lm,...mn->...in",
+            J,
+            R_cw,
+            cov,
+            R_cw.swapaxes(-1, -2),
+            J.swapaxes(-1, -2),
+        )
+
+        # Store covariance as scale and quaternion.
+        scale, quat_d = jnp.linalg.eigh(cov_d)
+        scale = jnp.sqrt(scale)
+        quat_d = jaxlie.SO2.from_matrix(quat_d)
+        g2d = Gaussians.from_props(
+            mean_d, quat_d, scale, gaussians.colors, gaussians.opacity
+        )
+
+        depth = t[..., 2]
+        assert g2d.get_and_check_shape() == 2
+        return g2d, depth
---
+++ src/splax/_gaussian_splat.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+import jax
+import jax_dataclasses as jdc
+import jaxlie
+
+import jax.numpy as jnp
+
+
+@jdc.pytree_dataclass
+class Gaussians:
+    means: jnp.ndarray
+    quat: jaxlie.SOBase
+    _scale: jnp.ndarray
+    _colors: jnp.ndarray
+    _opacity: jnp.ndarray
+
+    @staticmethod
+    def from_props(
+        means: jnp.ndarray,
+        quat: jaxlie.SOBase,
+        scale: jnp.ndarray,
+        colors: jnp.ndarray,
+        opacity: jnp.ndarray,
+    ) -> Gaussians:
+        _scale = jnp.log(scale)
+        _colors = jax.scipy.special.logit(colors)
+        _opacity = jax.scipy.special.logit(opacity)
+        return Gaussians(means, quat, _scale, _colors, _opacity)
+
+    @property
+    def scale(self) -> jnp.ndarray:
+        return jnp.exp(self._scale)
+
+    @property
+    def colors(self) -> jnp.ndarray:
+        return jax.nn.sigmoid(self._colors)
+
+    @property
+    def opacity(self) -> jnp.ndarray:
+        return jax.nn.sigmoid(self._opacity)
+
+    def get_and_check_shape(self) -> int:
+        n_dim = self.quat.space_dim
+        batch_axes = self.means.shape[:-1]
+        assert self.means.shape == (*batch_axes, n_dim)
+        assert self._scale.shape == (*batch_axes, n_dim)
+        assert self._colors.shape == (*batch_axes, 3)
+        assert self._opacity.shape == batch_axes
+        assert self.quat.get_batch_axes() == batch_axes
+        return n_dim
+
+    def get_bbox(self) -> jnp.ndarray:
+        cov_mat = self.quat.as_matrix()
+        # cov_mat = jnp.einsum("...ij,...j,...kj->...ik", cov_mat, self.scale ** 2, cov_mat)
+        # extent = 3 * jnp.abs(cov_mat).max(axis=-1)
+        extent = 3 * jnp.abs(cov_mat * self.scale[..., None, :]).max(axis=-1)
+        bbox = jnp.concatenate([self.means - extent, self.means + extent], axis=-1)
+        return bbox
+
+    @staticmethod
+    def from_random(n_gauss: int, prng_key: jax.Array, n_dim: int) -> Gaussians:
+        keys = jax.random.split(prng_key, 5)
+
+        means = jax.random.uniform(keys[0], (n_gauss, n_dim), minval=-1.0, maxval=1.0)
+        scale = jax.random.uniform(keys[1], (n_gauss, n_dim), minval=0.01, maxval=0.1)
+        colors = jax.random.uniform(keys[2], (n_gauss, n_dim), minval=0, maxval=1)
+        opacity = jax.random.uniform(keys[3], (n_gauss,), minval=0.5, maxval=1)
+
+        if n_dim == 3:
+            SOBase = jaxlie.SO3
+        elif n_dim == 2:
+            SOBase = jaxlie.SO2
+        else:
+            raise ValueError(f"Unsupported space dimension: {n_dim}")
+        quat = SOBase.sample_uniform(keys[4], (n_gauss,))
+
+        _scale = jnp.log(scale)
+        _colors = jax.scipy.special.logit(colors)
+        _opacity = jax.scipy.special.logit(opacity)
+
+        return Gaussians(means, quat, _scale, _colors, _opacity)
