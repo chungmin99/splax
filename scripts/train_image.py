fcde7811348b6b8873359b7e6473a5a3d7394e90
Chung Min Kim <chungmin99@gmail.com>
2025-01-03 00:25:02 -0800

    Add 2D+3D options for image training example

--- scripts/train_image.py
+++ scripts/train_image.py
@@ -10,8 +10,10 @@ import jax.numpy as jnp
 import numpy as onp
 import jax_dataclasses as jdc
 import optax
+import jaxlie
 
-from splax import Gaussian2D, rasterize
+from splax import Gaussian2D, Gaussian3D, rasterize, Camera
+
 
 @jdc.jit
 def normalize_to_image_space(
@@ -22,13 +24,18 @@ def normalize_to_image_space(
         _g2d._scale = jnp.log(g2d.scale * width)
     return _g2d
 
+
 def main(
     n_gauss: int = int(1e5),
     n_steps: int = 1000,
     scene: Literal["miffy", "sunset"] = "sunset",
+    mode: Literal["2D", "3D"] = "2D",
 ):
     """
     Simple gaussian splat optimization in image space.
+
+    Can optimize as "2D" (3D gaussians in SE2, with pre-determined rasterization ordering),
+    or as "3D" (3D gaussians in SE3, with perspective projection).
     """
     if scene == "miffy":
         target_img = plt.imread(Path(__file__).parent / "assets/miffy.jpeg")
@@ -42,39 +49,64 @@ def main(
     height, width, _ = target_img.shape
 
     prng_key = jax.random.PRNGKey(0)
-    g2d = Gaussian2D.from_random(n_gauss, prng_key)
 
-    img = rasterize(g2d, jnp.arange(n_gauss), height, width)
-    
+    if mode == "2D":
+        gaussians = Gaussian2D.from_random(n_gauss, prng_key)
+
+        def rasterize_gs(gs):
+            _gs = normalize_to_image_space(gs, height, width)
+            img = rasterize(_gs, jnp.arange(n_gauss), height, width)
+            return img
+
+    elif mode == "3D":
+        fx = fy = jnp.array(width / 4)
+        cx = cy = jnp.array(width / 2)
+        near = jnp.array(0.1)
+        far = jnp.array(1000.0)
+        pose = jaxlie.SE3.from_translation(jnp.array([0, 0, -1.5]))
+        camera = Camera.from_intrinsics(fx, fy, cx, cy, width, height, near, far, pose)
+        gaussians = Gaussian3D.from_random(n_gauss, prng_key)
+
+        def rasterize_gs(gs):
+            _gs_2d, depth = camera.project(gs)
+            img = rasterize(_gs_2d, depth, height, width)
+            return img
+
+    else:
+        raise ValueError(f"Invalid mode {mode}.")
+
     @jdc.jit
-    def loss_fn(g2d):
+    def loss_fn(gaussians):
+        img = rasterize_gs(gaussians)
         loss = jnp.abs(img - target_img).mean()
         return loss
 
     @jdc.jit
-    def step_fn(g2d, opt_state):
-        loss, grads = jax.value_and_grad(loss_fn)(g2d)
+    def step_fn(gaussians, opt_state):
+        loss, grads = jax.value_and_grad(loss_fn)(gaussians)
         updates, opt_state = optimizer.update(grads, opt_state)
-        g2d = cast(Gaussian2D, optax.apply_updates(g2d, updates))
-        g2d = g2d.fix()
-        return g2d, opt_state, loss
+        gaussians = optax.apply_updates(gaussians, updates)
+        gaussians = gaussians.fix()  # pyright: ignore
+        return gaussians, opt_state, loss
 
     # Initialize optimizer.
     optimizer = optax.chain(optax.clip(1e-1), optax.adam(learning_rate=1e-2))
-    opt_state = optimizer.init(cast(optax.Params, g2d))
+    opt_state = optimizer.init(cast(optax.Params, gaussians))
 
     # Training loop.
     result_imgs = []
     pbar = tqdm.trange(n_steps)
     for step in pbar:
-        g2d, opt_state, loss = step_fn(g2d, opt_state)
+        gaussians, opt_state, loss = step_fn(gaussians, opt_state)
         pbar.set_postfix(loss=f"{loss.item():.6f}")
         if step % 20 == 0:
+            img = rasterize_gs(gaussians)
+            plt.imsave("out.png", onp.array(img))
             result_imgs.append(img)
 
     # Save the result images as a gif.
     result_imgs = [onp.array(img * 255, dtype=onp.uint8) for img in result_imgs]
-    imageio.mimsave(f"{scene}_result.gif", result_imgs, fps=10, format="GIF", loop=0)  # pyright: ignore
+    imageio.mimsave(f"{scene}_result_{mode}.gif", result_imgs, format="GIF", loop=0, fps=10)  # pyright: ignore
 
 
 if __name__ == "__main__":
