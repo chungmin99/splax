4cd0eff0016d636ea2575339588e4dfda0e5e30d
Chung Min Kim <chungmin99@gmail.com>
2025-01-05 00:02:12 -0800

    Copy ssim from mipnerf

--- src/splax/__init__.py
+++ src/splax/__init__.py
@@ -2,3 +2,4 @@ from ._gaussian_splat import Gaussian3D as Gaussian3D
 from ._gaussian_splat import Gaussian2D as Gaussian2D
 from ._camera import Camera as Camera
 from ._rasterizer import rasterize as rasterize
+from ._utils import compute_ssim as compute_ssim
